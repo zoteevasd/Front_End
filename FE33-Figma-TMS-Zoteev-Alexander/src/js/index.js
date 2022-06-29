7b7aa332a8de7329b2fc3a2b5a6fd16528fcb492
Gitzoteevasd <zoteevasd@gmail.com>
2022-06-29 18:10:11 +0300

    Figma_TMS_slider

--- FE33-Figma-TMS-Zoteev-Alexander/src/js/index.js
+++ FE33-Figma-TMS-Zoteev-Alexander/src/js/index.js
@@ -1,1 +1,23 @@
-console.log("Aloha");
+console.log('Aloha')
+
+// Меню бургер
+
+const iconMenu = document.querySelector('.header-menu-burger');
+if (iconMenu){
+    const menuBody = document.querySelector('.header-menu');
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
+
+
+
+new Swiper('.image-slider',{
+    pagination: {
+        el: '.swiper-pagination',
+        clickable:true,
+        dynamicBullets: true,
+    },
+})
