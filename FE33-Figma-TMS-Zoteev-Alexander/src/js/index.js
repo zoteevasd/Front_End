7a6948cd24b24ce7435549dbb6eeb3a24b4d425a
Gitzoteevasd <zoteevasd@gmail.com>
2022-07-03 23:53:28 +0300

    >Figma_Scool1_portfolio

--- FE33-Figma-TMS-Zoteev-Alexander/src/js/index.js
+++ FE33-Figma-TMS-Zoteev-Alexander/src/js/index.js
@@ -4,21 +4,21 @@ console.log('Aloha')
 
 const iconMenu = document.querySelector('.header-menu-burger');
 if (iconMenu){
-    const menuBody = document.querySelector('.header-menu');
-    iconMenu.addEventListener('click', function (e) {
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    })
+	const menuBody = document.querySelector('.header-menu');
+	iconMenu.addEventListener('click', function (e) {
+		document.body.classList.toggle('_lock');
+		iconMenu.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	})
 }
 
 
 
 new Swiper('.image-slider',{
-    pagination: {
-        // el: '.swiper-scrollbar',
-        el: '.swiper-pagination',
-        clickable:true,
-        dynamicBullets: true,
-    },
+	pagination: {
+		// el: '.swiper-scrollbar',
+		el: '.swiper-pagination',
+		clickable:true,
+		dynamicBullets: true,
+	},
 })
